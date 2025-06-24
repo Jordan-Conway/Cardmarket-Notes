0cef9df862375d4ce9d1db476546eaa2a1550572
jordan-conway-0 <jccavan60@outlook.com>
2025-06-24 19:55:20 +0100

    Revert "Add initial content"
    This reverts commit 6ecc97bf568ee291da1095aad483e5d7eae4e39f.

--- showNotes.js
+++
@@ -1,7 +0,0 @@
-let desc = document.getElementsByClassName("py-3 px-1 p-md-3 col-12 col-md background-gray")[0]
-
-let notes = `<p>Note goes here</p>`
-
-desc.innerHTML = desc.innerHTML + notes
-
-console.log("Finished loading notes")
